15617aca4458c878b72da74ed5987f7e31f05b60
jitendra jha <jjha2010@gmail.com>
2020-11-25 11:26:55 +0530

    separated modules
    home page ,about page separated for further individula edits
    
    Co-Authored-By: saifiqbal01 <74584332+saifiqbal01@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component'
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: 'about', component: AboutComponent},
-  {path: '', component: AppComponent}
+  {path: '', component: AppComponent},
+  {path:'home', component:HomeComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
+    HomeComponent
     
   ],
   imports: [
