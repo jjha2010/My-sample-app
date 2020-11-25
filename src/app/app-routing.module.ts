5d1e53beb586c0b5ee479d4ea28024ed7950ccfa
saifiqbal01 <74584332+saifiqbal01@users.noreply.github.com>
2020-11-25 10:22:14 +0530

    about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component'
+import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  {path: 'about', component: AboutComponent},
+  {path: '', component: AppComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
